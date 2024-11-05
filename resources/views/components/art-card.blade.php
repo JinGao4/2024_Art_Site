d6bbd67f8639d91719ea8f7d49db9aa1c58d9d4c
JinGao4 <157361689+JinGao4@users.noreply.github.com>
2024-11-05 23:20:37 +0000

    finishing view all art & creating component

--- resources/views/components/art-card.blade.php
+++ resources/views/components/art-card.blade.php
@@ -1,3 +1,8 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
+@props(['title','image','artistname','about'])
+
+<div class="border rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition duration-300">
+    <h4 class="font-bold text-lg">{{$title}}</h4>
+    <img src="{{$image}}" alt="{{$title}}">
+    <p class="text-gray-600">{{$artistname}}</p>
+    <p class="text-gray-800 mt-4">({{$about}})</p>
 </div>
