571048e78bc6e94b683e3a5ab731be29298f3146
JinGao4 <157361689+JinGao4@users.noreply.github.com>
2024-11-11 12:21:21 +0000

    added comment

--- resources/views/components/alerts-success.blade.php
+++ resources/views/components/alerts-success.blade.php
@@ -1,9 +1,13 @@
+
+<!--  Check the 'success' message stored in the session -->
 @if(session('success'))
 <div class="mb-4 px-4 py-2 bg-green-100 border-green-500 text-green-700 rounded-md">
+<!-- Output the success message from the session -->
     {{$slot}}
 </div>
 @endif
 
+<!-- Custom Blade component that displays a success alert -->
 <x-alert-success>
     {{session('success')}}
 </x-alert-success>
