05c9ad2ef9801278a38675b3abe022e25205fedd
JinGao4 <157361689+JinGao4@users.noreply.github.com>
2024-11-12 15:17:21 +0000

    the seeder is working

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::Create([
+            'name'=>'Admin User',
+            'email'=>'admin'.time().'@example.com',
+            'password'=>Hash::make('password'),
+            'role'=>'admin',
+        ]);
     }
 }
