2050c64bc5cdaa35c5650efc52bbd773958fccca
JinGao4 <157361689+JinGao4@users.noreply.github.com>
2024-12-03 14:54:38 +0000

    didnt add the folder for the review and added it

--- resources/views/reviews/edit.blade.php
+++ resources/views/reviews/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name='header'>
         <h2 class="font-semibold text-xl text-gray-800 leading-tight ">
-            {{ __('Edit art') }}
+            {{ __('Edit Review') }}
         </h2>
     </x-slot>
 
@@ -9,18 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h3 class="front-semibold text-lg mb-4">Edit Art piece:</h3>
-                    <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
-                        <x-art-form
-                            :action="route('arts.update',$art)"
+                    <h3 class="front-semibold text-lg mb-4">Edit Review:</h3>
+                    
+                        <x-review-form
+                            :action="route('reviews.update',$review)"
                             :method="'PUT'"
-                            :art="$art"
+                            :art="$review"
                         />
-                    </div>
+                    
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
-
-
